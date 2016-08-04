64d0f6e2cc3a3b9759d518d18c12c334a5af8df9
Piotr Bandurski <piotr.bandurski@intel.com>
2016-08-04 16:26:54 +0200

    random forest + cross validation

--- test1.py
+++ test1.py
@@ -6,18 +6,37 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-from sklearn import cross_validation
+from sklearn import cross_validation as cv
+from sklearn.ensemble import RandomForestClassifier
 
 dataset = pd.read_csv("data/train.csv")
 
-#print dataset.head()
+print dataset.shape
 
-labels = dataset.iloc[:, 0]
-train = dataset.iloc[:, 1:]
+train, test = cv.train_test_split(dataset, train_size = 0.8)
 
-print labels.shape
+train_Y = train.iloc[:, 0]
+train_X = train.iloc[:, 1:]
 
-print train.shape
+val_Y = test.iloc[:, 0]
+val_X = test.iloc[:, 1:]
+
+print train_X.shape, train_Y.shape
+print val_X.shape, val_Y.shape
+
+rf = RandomForestClassifier(n_estimators=100, n_jobs=8)
+rf.fit(train_X, train_Y)
+pred = rf.predict(val_X)
+
+result =  float(sum(pred == val_Y)) / val_Y.shape[0]
+
+print "%.2f%%" %(result * 100)
+
+scores = cv.cross_val_score(rf, train_X, train_Y, cv=5)
+
+print scores
+
+#np.savetxt('submission_rand_forest.csv', np.c_[range(1,len(test)+1),pred], delimiter=',', header = 'ImageId,Label', comments = '', fmt='%d')
 
 
 
